94db43932d1f3f2bcf3733aae30efa99ef8745ad
MajidKh230 <khanmajid230@gmail.com>
2024-12-23 17:14:03 +0500

    initial commit

---
+++ Backend/index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+
+const app = express()
+dotenv.config()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.status(200).send({
+        messsage:'welcome to Backend'
+    })
+})
+
+
+
+
+
+export default app
