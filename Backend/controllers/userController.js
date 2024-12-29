dc2a39b29d2e5b4e0e8011847b2a42bc7792b4f2
MajidKh230 <khanmajid230@gmail.com>
2024-12-29 16:11:38 +0500

    redefine user controller

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,92 +1,62 @@
 import userModel from "../models/userModel.js";
-import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken'
+
+
+
+// generate auth token
+const generateAuthToken = async function(email){
+  const token = await jwt.sign(email,process.env.SECRET_KEY)
+  return token
+} 
+
+// hashPassword
+const hashPassword = async function(password){
+  return await bcrypt.hash(password,10)
+}
+
+//compare password
+const comparePassword = async function(plainPassword, hashedPassword){
+  return await bcrypt.compare(plainPassword, hashedPassword)
+}
+
+
+
 const register = async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  if (!fullName || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const isUserExist = await userModel.findOne({ email });
+
+  // console.log(isUserExist)
+
+  if (isUserExist) {
+    return  res.status(400).json({ message: "User already exists" });
+  }
+
+  const hashedPassword = await hashPassword(password);
+
+  const user = await userModel.create({
+    fullName,
+    email,
+    password: hashedPassword,
+  });
+
   try {
-    const { fullName, email, password } = req.body;
-    if (!fullName || !email || !password) {
-      res.status(200).json("All fields are required!");
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const compPassword = await bcrypt.compare(password, hashedPassword);
-    console.log(compPassword);
-
-    const token = jwt.sign(email, process.env.SECRET_KEY);
-    console.log(token);
-
-    const user = await userModel.create({
-      fullName,
-      email,
-      password: hashedPassword,
-    });
-
-    if (!user) {
-      res
-        .status(400)
-        .json({ message: "User registration faild please try again" });
-    }
-
-    res.cookie("token", token);
-
-    //   console.log(jwt.verify(token,process.env.SECRET_KEY))
-
-    res.status(201).json({
-      message: "user sucessfully created!",
-      user: user,
-      token: token,
-    });
+    res.status(200).json({ message: "User created successfully", user });
   } catch (error) {
-    res.status(400).send({
-      message: "something went wrong with server",
+    res.status(404).send({
+      message: "something went wrong",
       error: error.message,
     });
   }
 };
-const login = async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await userModel.findOne({ email });
-
-  console.log(user);
-
-  const isValidatePassword = await bcrypt.compare(password, user?.password);
-  console.log(isValidatePassword);
-
-  if (!isValidatePassword) {
-    return res.status(400).json({
-      message: "invalid credentials",
-    });
-  }
-
-
-res.status(200).json({message:"login sucessfully"})
-
-  // const { token } = req.cookies;
-  // console.log(token);
-  // try {
-  //   const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
-
-  //   if (!verifyToken) {
-  //      res.status(401).send({
-  //       message: "Unauthorized User",
-  //     });
-
-  //     res.status(200).send({
-  //       message: "user successfully login!",
-  //       error: error.message,
-  //     });
-  //   }
-  // } catch (error) {
-  //   res.status(400).send({
-  //     message: "something went wrong with server",
-  //     error: error.message,
-  //   });
-  // }
-};
-const logout = () => {};
-const forgotPassword = () => {};
+const login = (req, res) => {};
+const logout = (req, res) => {};
+const forgotPassword = (req, res) => {};
 
 const userController = { register, login, logout, forgotPassword };
 
